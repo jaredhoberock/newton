602aadf029717436a425b53bd2cc68d53e6f7a12
Jared Hoberock <jaredhoberock@gmail.com>
2012-04-14 19:46:05 -0700

    Full saxpy program working

--- examples/saxpy.cu
+++ examples/saxpy.cu
@@ -1,7 +1,9 @@
 #include <thrust/device_vector.h>
 #include <newton/numeric_range.hpp>
+#include <newton/numeric_vector.hpp>
+#include <thrust/copy.h>
 #include <iostream>
-#include <typeinfo>
+#include <iterator>
 
 int main()
 {
@@ -10,17 +12,23 @@ int main()
   float h_vec_y[4] = {1.0, 2.0, 3.0, 4.0};
 
   thrust::device_vector<float> d_vec_x(h_vec_x,h_vec_x + 4);
-  thrust::device_vector<float> d_vec_y(h_vec_y,h_vec_y + 4);
 
   typedef thrust::device_vector<float>::iterator iterator;
 
-  // create a numeric "view" of the vectors
+  // create a numeric "view" vector of vector x
   newton::numeric_range<iterator> x = d_vec_x;
-  newton::numeric_range<iterator> y = d_vec_y;
+
+  // create a numeric_vector (a container) from h_vec_y
+  newton::numeric_vector<float> y(h_vec_y, h_vec_y + 4);
 
   const float a = 2.0f;
   
-  a * x + y;
+  // saxpy to y 
+  y = a * x + y;
+
+  std::cout << "y: ";
+  thrust::copy(y.begin(), y.end(), std::ostream_iterator<float>(std::cout, " "));
+  std::cout << std::endl;
 
   return 0;
 }
