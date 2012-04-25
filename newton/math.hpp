d64e3b1bd1640812ba7209515b4562ae198de5de
Jared Hoberock <jaredhoberock@gmail.com>
2012-04-24 23:54:08 -0700

    Use the range operation macros in newton/math.hpp

--- newton/math.hpp
+++ newton/math.hpp
@@ -1,401 +1,37 @@
 #pragma once
 
-#include <newton/detail/range/range_traits.hpp>
+#include <newton/detail/define_range_function.hpp>
 #include <newton/detail/functional.hpp>
-#include <newton/detail/range/constant_range.hpp>
 
 namespace newton
 {
 
 
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::absolute_value<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    abs(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::absolute_value<value_type>());
-} // end abs
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(abs, detail::absolute_value);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(acos, detail::arc_cosine);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(asin, detail::arc_sine);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(atan, detail::arc_tangent);
+__NEWTON_DEFINE_BINARY_RANGE_FUNCTION(atan2, detail::arc_tangent2);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(cos, detail::cosine);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(cosh, detail::hyperbolic_cosine);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(erf, detail::error_function);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(exp, detail::exponential);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(log, detail::logarithm);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(log10, detail::logarithm10);
+__NEWTON_DEFINE_BINARY_RANGE_FUNCTION(pow, detail::power);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(sin, detail::sine);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(sinh, detail::hyperbolic_sine);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(sqrt, detail::square_root);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(tan, detail::tangent);
+__NEWTON_DEFINE_UNARY_RANGE_FUNCTION(tanh, detail::hyperbolic_tangent);
 
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::arc_cosine<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    acos(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::arc_cosine<value_type>());
-} // end acos
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::arc_sine<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    asin(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::arc_sine<value_type>());
-} // end asin
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::arc_tangent<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    atan(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::arc_tangent<value_type>());
-} // end atan
-
-
-template<typename NumericRange1, typename NumericRange2>
-inline __host__ __device__
-  typename detail::lazy_enable_if_ranges<
-    NumericRange1, NumericRange2,
-    detail::transform2_result<
-      const NumericRange1,
-      const NumericRange2,
-      detail::arc_tangent2<
-        typename detail::range_value<NumericRange1>::type
-      >
-    >
-  >::type
-    atan2(const NumericRange1 &rng1, const NumericRange2 &rng2)
-{
-  typedef typename detail::range_value<NumericRange1>::type value_type1;
-  return detail::transform(rng1, rng2, detail::arc_tangent2<value_type1>());
-} // end atan2
-
-
-template<typename NumericRange, typename T>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range_and_scalar<
-    NumericRange, T,
-    detail::transform2_result<
-      const NumericRange,
-      detail::constant_range<T>,
-      detail::arc_tangent2<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    atan2(const NumericRange &rng, const T &c)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, make_constant_range(c, rng.size()), detail::arc_tangent2<value_type>());
-} // end atan2
-
-
-template<typename T, typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_scalar_and_range<
-    T, NumericRange,
-    detail::transform2_result<
-      detail::constant_range<T>,
-      const NumericRange,
-      detail::arc_tangent2<
-        typename detail::range_value<T>::type
-      >
-    >
-  >::type
-    atan2(const T &c, const NumericRange &rng)
-{
-  typedef typename detail::range_value<T>::type value_type;
-  return detail::transform(make_constant_range(c, rng.size()), rng, detail::arc_tangent2<value_type>());
-} // end atan2
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::cosine<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    cos(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::cosine<value_type>());
-} // end cos
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::hyperbolic_cosine<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    cosh(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::hyperbolic_cosine<value_type>());
-} // end cosh
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::error_function<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    erf(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::error_function<value_type>());
-} // end erf
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::exponential<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    exp(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::exponential<value_type>());
-} // end exp
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::logarithm<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    log(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::logarithm<value_type>());
-} // end log
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::logarithm10<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    log10(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::logarithm10<value_type>());
-} // end log10
-
-
-template<typename NumericRange1, typename NumericRange2>
-inline __host__ __device__
-  typename detail::lazy_enable_if_ranges<
-    NumericRange1, NumericRange2,
-    detail::transform2_result<
-      const NumericRange1,
-      const NumericRange2,
-      detail::power<
-        typename detail::range_value<NumericRange1>::type
-      >
-    >
-  >::type
-    pow(const NumericRange1 &rng1, const NumericRange2 &rng2)
-{
-  typedef typename detail::range_value<NumericRange1>::type value_type1;
-  return detail::transform(rng1, rng2, detail::power<value_type1>());
-} // end pow
-
-
-template<typename NumericRange, typename T>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range_and_scalar<
-    NumericRange, T,
-    detail::transform2_result<
-      const NumericRange,
-      detail::constant_range<T>,
-      detail::power<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    pow(const NumericRange &rng, const T &c)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, make_constant_range(c, rng.size()), detail::power<value_type>());
-} // end pow
-
-
-template<typename T, typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_scalar_and_range<
-    T, NumericRange,
-    detail::transform2_result<
-      detail::constant_range<T>,
-      const NumericRange,
-      detail::arc_tangent<
-        typename detail::range_value<T>::type
-      >
-    >
-  >::type
-    pow(const T &c, const NumericRange &rng)
-{
-  typedef typename detail::range_value<T>::type value_type;
-  return detail::transform(make_constant_range(c, rng.size()), rng, detail::power<value_type>());
-} // end pow
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::sine<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    sin(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::sine<value_type>());
-} // end sin
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::hyperbolic_sine<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    sinh(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::hyperbolic_sine<value_type>());
-} // end sinh
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::square_root<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    sqrt(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::square_root<value_type>());
-} // end sqrt
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::tangent<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    tan(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::tangent<value_type>());
-} // end tan
-
-
-template<typename NumericRange>
-inline __host__ __device__
-  typename detail::lazy_enable_if_range<
-    NumericRange,
-    detail::transform1_result<
-      const NumericRange,
-      detail::hyperbolic_tangent<
-        typename detail::range_value<NumericRange>::type
-      >
-    >
-  >::type
-    tanh(const NumericRange &rng)
-{
-  typedef typename detail::range_value<NumericRange>::type value_type;
-  return detail::transform(rng, detail::hyperbolic_tangent<value_type>());
-} // end tanh
 
 // mirror these functions in namespace detail so that they are
 // available to intermediate range expressions such as
 // detail::transform_range and detail::constant_range
 namespace detail
 {
+
 
 using newton::abs;
 using newton::acos;
@@ -415,8 +51,7 @@ using newton::sqrt;
 using newton::tan;
 using newton::tanh;
 
+
 } // end detail
-
-
 } // end newton
 
